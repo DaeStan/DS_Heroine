9d6273cd2ba3f78c9071db5874a742c4cd0727ac
DaeStan <stanifed@my.erau.edu>
2024-09-09 10:40:40 -0700

    Base States and Player implemented

---
+++ DS_Heroine_PR/Assets/States/StandingPlayerState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingPlayerState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        Debug.Log("Entering State: Standing");
+        player.mCurrentState = this;
+    }
+
+    public void Execute(Player player)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // transition to jump
+            JumpingPlayerState jumpingState = new JumpingPlayerState();
+            jumpingState.Enter(player);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // transition to duck
+            DuckingPlayerState duckingState = new DuckingPlayerState();
+            duckingState.Enter(player);
+        }
+    }
+}
